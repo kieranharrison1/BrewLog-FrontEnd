e4881b11dba11a14b054e7d502494970628e0f1e
KH-Vald22 <Kieran.Harrison22@hotmail.co.uk>
2025-05-27 10:48:58 +1000

    Refactor Pt.1

--- README.md
+++
@@ -1,1 +0,0 @@
-# BrewLog-FrontEnd
--- main.js
+++
@@ -1,6 +0,0 @@
-import App from './src/app.js'
-import '@shoelace-style/shoelace/dist/shoelace.js';
-
-const app = new App();
-window.addEventListener('load', () => app.init());
-window.addEventListener('hashchange', () => app.route());
